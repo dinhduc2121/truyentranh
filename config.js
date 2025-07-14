41e3cc8098af7d2573b9a90d847130d361aa6983
Nguyen Dinh Duc <genshin01022002@gmail.com>
2025-07-14 18:03:33 +0700

    v4

--- config.js
+++ config.js
@@ -2,6 +2,3 @@ export const API_BASE_URL =
   import.meta.env.MODE === "development"
     ? `${import.meta.env.VITE_API_HOST}:${import.meta.env.VITE_API_PORT}${import.meta.env.VITE_API_PATH}`
     : "https://backend-production-3828.up.railway.app";
-
-// Sử dụng:
-fetch(`${API_BASE_URL}/api/home`);
