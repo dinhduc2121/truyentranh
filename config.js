c259014aaba9956ad53e8483da79adce67d2ed41
Nguyen Dinh Duc <genshin01022002@gmail.com>
2025-07-15 09:03:48 +0700

    v22

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
 export const API_BASE_URL = 
   import.meta.env.MODE === "development"
     ? "http://localhost:8080"
-    
-    : "http://backend-production-b688.up.railway.app";
+    : "https://backend-production-b688.up.railway.app";
